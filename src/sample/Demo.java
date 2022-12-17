1dc0fe30e0bd47cba91d874cf32f10453a026415
hp <hp@Smeeti>
2022-12-17 09:41:49 +0530

    this is fiest change

--- src/sample/Demo.java
+++ src/sample/Demo.java
@@ -3,7 +3,14 @@ package sample;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	  int a=10;
+	  int b=20;
+	  
+	  
+	  int sum=a+b;
+	  
+	  
+	  System.out.println(sum);
 
 	}
 
